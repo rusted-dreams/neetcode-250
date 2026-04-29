6e90ef9b7e6251cb5a94434b58ec465aa66499e7
shubhomkar <123829783+rusted-dreams@users.noreply.github.com>
2026-04-29 15:15:32 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> count(n+1, 0);
+        for(int i = 0; i< trust.size(); i++) {
+            count[trust[i][1]]++;
+            count[trust[i][0]]--;
+        }
+        for(int i =1; i<count.size(); i++) {
+            if(count[i] == n-1) return i;
+        }
+        return -1;
+    }
+};
